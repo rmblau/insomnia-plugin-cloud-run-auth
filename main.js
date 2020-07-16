7c7fbd79470d3906221218ca7eaaca5123b06b5f
nawhi <25394965+nawhi@users.noreply.github.com>
2020-07-16 14:26:35 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,28 @@
+const {exec} = require("child_process");
+
+tokenTemplate = {
+    name: 'cloudRunIdToken',
+    displayName: "Google Cloud Run Identity Token",
+    description: "Generate an identity token for calling authenticated Google Cloud Run endpoints",
+    args: [
+        {
+            displayName: "Path to Google Cloud CLI binary",
+            type: 'string',
+            defaultValue: `${process.env.HOME}/google-cloud-sdk/bin/gcloud`
+        }
+    ],
+    run: async (_, gcloud) => {
+        return await new Promise((resolve, reject) => {
+            exec(`${gcloud} auth --quiet print-identity-token`, (err, stdout, stderr) => {
+                if (err) reject(err);
+                resolve(stdout.toString().trim());
+            });
+        });
+    }
+}
+
+module.exports.templateTags = [tokenTemplate];
+
+if (typeof require !== 'undefined' && require.main === module) {
+    tokenTemplate.run().then(console.log);
+}
